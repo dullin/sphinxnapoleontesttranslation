a48391471c47a8d759bd289e84dedb10f3da2381
Hugo Leblanc <dullin@gmail.com>
2018-03-27 10:54:16 -0400

    Initial commit.

---
+++ _build/html/_sources/index.rst.txt
@@ -0,0 +1,21 @@
+.. testtranslate documentation master file, created by
+   sphinx-quickstart on Mon Mar 26 11:46:41 2018.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to testtranslate's documentation!
+=========================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+
+.. autofunction:: func.func
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
---
+++ src/func.py
@@ -0,0 +1,48 @@
+def func(arg1, arg2):
+    """Summary line.
+
+    Extended description of function.
+
+    Args:
+        arg1 (int): Description of arg1
+        arg2 (str): Description of arg2
+
+    Arguments:
+        arg1 (int): Description of arg1
+        arg2 (str): Description of arg2
+
+    Attributes:
+        some attribs
+
+    Example:
+        ::
+
+            func(3,2)
+
+    Examples:
+        Another example.
+
+    Keyword Args:
+        this
+        that
+
+    Returns:
+        bool: Description of return value
+
+    Raises:
+        Errors out to bad stuff.
+
+    References:
+        A reference.
+    
+    See also:
+        Some more code
+
+    Warning:
+        Faire attention à ça.
+
+    Yield:
+        Some yeilding.
+
+    """
+    return True
